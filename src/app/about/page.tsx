2e9ea72421a1d3ae50ec80d035de11d268ac7e57
tobennaUmeh <94992053+tobennaUmeh@users.noreply.github.com>
2023-06-25 01:20:28 +0100

    portfolio website recent work

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,12 @@
+import Link from 'next/link';
 import React from 'react';
 
 const about = () => {
-	return <div>about</div>;
+	return (
+		<div>
+			<Link href='/'>About</Link>
+		</div>
+	);
 };
 
 export default about;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,7 +16,35 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				<div className='flex justify-center'>
+					<Link
+						className='mx-4 my-3 group rounded-lg border border-transparent px-2 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30:'
+						href='/'
+					>
+						Home
+					</Link>
+					<Link
+						className='mx-4 my-3 group rounded-lg border border-transparent px-2 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30:'
+						href='/about'
+					>
+						About
+					</Link>
+					<Link
+						className='mx-4 my-3 group rounded-lg border border-transparent px-2 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30:'
+						href='/about/clients'
+					>
+						Clients
+					</Link>
+					<Link
+						className='mx-4 my-3 group rounded-lg border border-transparent px-2 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30:'
+						href='/about/clients'
+					>
+						Featured works
+					</Link>
+				</div>
+				{children}
+			</body>
 		</html>
 	);
 }
